d737f58bdea73d9b2126737600ef2e61424e6de0
Chris Ege <cege@wttw.com>
2013-02-15 16:34:49 -0600

    added form to create new items

--- public/javascripts/list-express.js
+++ public/javascripts/list-express.js
@@ -3,69 +3,6 @@ var listExpress = listExpress || {};
 
 
 $(function() {
-	// var item = new listExpress.Item({
-	// 		title: "first item"
-	// 	});
-   // Kick things off by creating the **App**.
-   // var itemView = new listExpress.ItemView({
-   // 	model: item
-   //    });
-   // 	$("#itemContainer").html(itemView.render().el);
 	new listExpress.AppView();
-	//window.listExpress.Items.create({title:"first item"});
-
 });
-// var Item = Backbone.Model.extend({
-// 	defaults: {
-// 		title: "",
-// 		completed: false,
-// 		category: ""			
-// 	},
-// 	toggle: function() {
-//       this.save({
-//         completed: !this.get('completed')
-//       });
-//     }
-// });
 
-
-// var ItemList = Backbone.Collection.extend({
-// 	model: Item,
-// 	
-// 	completed: function(){
-// 		return this.filter(function(item){
-// 			return item.get('completed');
-// 		});
-// 	},
-// 	
-// 	remaining: function(){
-// 		return this.without.apply(this, this.completed());
-// 	}
-// 	
-// });
-
-// var ItemView = Backbone.View.extend({
-// 	tagName: "div",
-// 	className: "itemContainer",
-// 	template: $("#itemTemplate").html(),
-// 	render: function(){
-// 		var tmpl = _.template(this.template);
-// 		this.$el.html(tmpl(this.model.toJSON()));
-// 		return this;
-// 	}
-// });
-
-
-// var item = new Item({
-// 	title: "first item"
-// });
-// 
-// 
-// var itemView = new ItemView({
-// 	model:item
-// });
-// 
-// 
-// $('document').ready(function(){
-// 	$("#itemContainer").html(itemView.render().el);
-// });
--- public/javascripts/views/app.js
+++ public/javascripts/views/app.js
@@ -4,10 +4,10 @@ listExpress.AppView = Backbone.View.extend({
 	el: "#listExpress",
 	
 	initialize: function(){
+		this.input = this.$('#newItemForm');
+		console.log(this.input);
 		window.listExpress.Items.on('reset', this.renderAll, this);
 		window.listExpress.Items.on('add', this.renderItem, this);
-		//window.listExpress.Items.on('all', this.render, this); 
-		//this.listenTo(listExpress.Items, "change", this.render);		
 		listExpress.Items.fetch();
 	},
 	
@@ -17,6 +17,10 @@ listExpress.AppView = Backbone.View.extend({
 		_.each(listExpress.Items.models, function(item){
 			that.renderItem(item);
 		}, this);
+	},
+	
+	events: {
+		'click #newItemSave': 'create',
 	},
 	
 	renderItem: function(item){
@@ -32,6 +36,19 @@ listExpress.AppView = Backbone.View.extend({
 		_.each(listExpress.Items.models, function(item){
 			that.renderItem(item);
 		}, this);
-	}
+	},
+	
+	newAttributes: function() {
+      return {
+        title: $(this.input).children('#newItemTitle').val().trim(),
+	    category: this.input.children("#newItemCategory").val().trim(),
+        completed: false
+      };
+    },
+	
+	create: function(){
+		listExpress.Items.create(this.newAttributes());
+		this.input.children('input').val('');
+	},
 	
 });
